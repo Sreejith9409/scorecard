68894d72543806c09cfbacd48e26a61fc6070b21
Sreejith <sreejith.s@bitlasoft.com>
2021-04-23 08:01:12 +0530

    Made minor changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,36 +1,36 @@
 require 'csv'
-class Student
+module Student
 
   attr_accessor :students, :agrade_students, :bgrade_students, :cgrade_students, :above_avg_students, :below_avg_students
 
-  def Student.get_student_report
-    Student.construct_data
-    Student.export_to_csv
+  def self.get_student_report
+    self.construct_student_data
+    self.export_to_csv
   end
 
   private
 
-  def Student.construct_data
+  def self.construct_student_data
     file_data = File.read("input.txt").split
     @students ||= []
     file_data.each do |f_data|
       data = f_data.split(",")
       id = data[0]
-      s1 = Student.get_student_mark(data[1])
-      s2 = Student.get_student_mark(data[2])
-      s3 = Student.get_student_mark(data[3])
-      s4 = Student.get_student_mark(data[4])
+      s1 = self.get_student_mark(data[1])
+      s2 = self.get_student_mark(data[2])
+      s3 = self.get_student_mark(data[3])
+      s4 = self.get_student_mark(data[4])
       total = (s1 + s2 + s3 + s4)
-      @students << [data[0], s1, s2, s3, s4, total, Student.calculate_grade(total)]
+      @students << [data[0], s1, s2, s3, s4, total, self.calculate_grade(total)]
     end
-    Student.find_category_of_students
+    self.find_category_of_students
   end
 
-  def Student.get_student_mark(data)
+  def self.get_student_mark(data)
     data.to_s.split('-')[1].to_f
   end
 
-  def  Student.calculate_grade(total)
+  def  self.calculate_grade(total)
     grade = if total >= 340
       'A'
     elsif total >= 300 and total < 340
@@ -40,19 +40,19 @@ class Student
     end
   end
 
-  def Student.find_category_of_students
-    avg_a, avg_b, avg_c = Student.calculate_grade_avg
+  def self.find_category_of_students
+    avg_a, avg_b, avg_c = self.calculate_grade_avg
     @below_avg_students = @above_avg_students = 0
     @students.each do |s|
       case s[6]
-      when 'A' then s << Student.compare_average(s[5], avg_a)
-      when 'B' then s << Student.compare_average(s[5], avg_b)
-      when 'C' then s << Student.compare_average(s[5], avg_c)
+      when 'A' then s << self.compare_average(s[5], avg_a)
+      when 'B' then s << self.compare_average(s[5], avg_b)
+      when 'C' then s << self.compare_average(s[5], avg_c)
       end
     end
   end
 
-  def Student.compare_average(total, grade_average)
+  def self.compare_average(total, grade_average)
     if total >= grade_average
       grade = 'ABOVE'
       @above_avg_students += 1
@@ -63,7 +63,7 @@ class Student
     grade
   end
 
-  def Student.calculate_grade_avg
+  def self.calculate_grade_avg
     avg_a = avg_b = avg_c = a_sum = b_sum = c_sum = 0.0
     @agrade_students = @bgrade_students = @cgrade_students = 0
     @students.each do |s|
@@ -84,7 +84,7 @@ class Student
     [avg_a, avg_b, avg_c]
   end
 
-  def Student.export_to_csv
+  def self.export_to_csv
     csv = CSV.generate do |csv|
       csv << ["ID", "Subject1", "Subject2", "Subject3", "Subject4", "Grand-Total", "Grade", "Average-Compare"]
       print " ID | Subject 1 | Subject 2 | Subject 3 | Subject 4 | Grand-Total | Grade | Average-Compare |\n"
